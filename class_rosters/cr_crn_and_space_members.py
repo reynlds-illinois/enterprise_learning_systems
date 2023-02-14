f85c945e03efd59f4acbc477fa3b60e93ea76cfa
Reynolds, Mark A <2440061+reynlds-illinois@users.noreply.github.com>
2023-02-14 08:59:05 -0600

    Create cr_crn_and_space_members.py
    This script queries the class rosters API and returns member enrollments based on CRN or space ID.

---
+++ class_rosters/cr_crn_and_space_members.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+#
+import sys,logging,requests,urllib, ldap3, json, cx_Oracle
+from ldap3 import Server, Connection, ALL
+sys.path.append("/var/lib/canvas-mgmt/bin")
+from canvasFunctions import getEnv
+from canvasFunctions import batchLookupReg
+from canvasFunctions import bind2Ldap
+from canvasFunctions import connect2Sql
+from canvasFunctions import logScriptStart
+#
+envDict = getEnv()
+logScriptStart()
+crHost = envDict['class.rosters.host']
+crXapikey = envDict['class.rosters.xapi']
+ldapHost = envDict['UofI.ldap.ad_sys']
+ldapBindDn = envDict['UofI.ad_bind']
+ldapBindPw = envDict['UofI.ad_bindpwd']
+ldapSearchBase = envDict['ad-prod.searchbase_new']
+dbUser = envDict['req-prod.db.user']
+dbPass = envDict['req-prod.db.pass']
+dbHost = envDict['req-prod.db.sys']
+dbPort = 1521
+dbSid = envDict['req-prod.db.sid']
+crns = []
+lookupChoice = ''
+spaceID = []
+eXit = 0
+choice = ''
+#
+ldapConn = bind2Ldap(ldapHost, ldapBindDn, ldapBindPw)
+cursor = connect2Sql(dbUser, dbPass, dbHost, dbPort, dbSid)
+print('')
+#
+while lookupChoice != 's' and lookupChoice != 'c':
+    lookupChoice = input('Lookup by (s)pace or (c)rn? ').lower()
+print('')
+#
+while eXit == 0:
+    if lookupChoice == 'c':
+        crn = input ('Enter CRN: ')
+        termcode = input('Enter Banner Term ID: ')
+        spaceQuery = f"""select SR.TARGET_PRODUCT_KEY || TM.BANNER_PART_OF_TERM || '_' || SR.SPACE_ID CID
+            from CORREL.T_SPACE_ROSTER SRR
+                    join CORREL.T_SPACE_REQUEST SR on(SRR.SPACE_ID = SR.SPACE_ID)
+                    left join CORREL.T_TERM TM on(SR.TERM_ID = TM.TERM_ID)
+            where SRR.TERM_CODE = {termcode}
+                    and SR.PRODUCT_ID = 'BB9'
+                    and SRR.ROSTER_DATA_SOURCE_KEY = {crn}"""
+        cursor.execute(spaceQuery)
+        spaceInfo = cursor.fetchall()
+        if len(spaceInfo) > 0:
+            print(f'Spaces using CRN {crn}: ')
+            for item in spaceInfo:
+                print('     ',str(item).strip("',()"))
+                spaceID.append(str(item).strip("',()").split('_')[-1])
+            while choice != 'y' and choice != 'n':
+                choice = input('Would you like detailed space and member info? (y/n)').lower()
+            if choice == 'n':
+                print('Exiting...')
+                eXit += 1
+                break
+        else:
+            print(f"No spaces found for CRN {crn}.")
+            eXit += 1
+            break
+    else:
+        spaceIDinput = input('Enter space ID: ')
+        spaceID.append(spaceIDinput)
+    #
+    for item in spaceID:
+        print(f'========== DETAILED INFO FOR SPACE {item} ==========')
+        crnQuery = f"""SELECT SRR.ROSTER_DATA_SOURCE_KEY CRN,
+               SRR.ROSTER_DATA_SOURCE_KEY CRN,
+               ADR.RUBRIC,
+               ADR.COURSE_NUMBER,
+               ADR.SECTION,
+               ADR.TERM_CODE TERM
+        FROM CORREL.T_SPACE_ROSTER SRR
+          LEFT JOIN CORREL.T_AD_ROSTER ADR on(SRR.ROSTER_DATA_SOURCE_KEY = ADR.CRN and SRR.TERM_CODE = ADR.TERM_CODE)
+        WHERE SRR.SPACE_ID = {item}"""
+        cursor.execute(crnQuery)
+        crnInfo = cursor.fetchall()
+        #
+        for item in range(0, len(crnInfo)):
+            crns.append(crnInfo[item][0])
+        termcode = crnInfo[0][5]
+        #
+        r = batchLookupReg(crHost, crXapikey, termcode, crns)
+        print('')
+        for c in r:
+            print(f"CRN: {c} - term: {termcode}")
+            for l in (r[c]):
+                print('    ', l)
+                for uin in r[c][l]:
+                    adFilter = '(&(objectclass=user)(uiucEduUIN=' + uin  + '))'
+                    bitBkt = ldapConn.search(search_base=ldapSearchBase, search_filter=adFilter,
+                        attributes = ['sAMAccountName', 'displayName', 'uiucEduUIN'], size_limit=0)
+                    temp = json.loads(ldapConn.response_to_json())
+                    netId = temp['entries'][0]['attributes']['sAMAccountName']
+                    displayName = temp['entries'][0]['attributes']['displayName']
+                    print('        ', netId, '-', displayName)
+        print('')
+        eXit += 1
