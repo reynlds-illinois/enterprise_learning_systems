4bfd9bb55ab0cb41444f9f6995e8d189755af15f
Reynolds, Mark A <2440061+reynlds-illinois@users.noreply.github.com>
2023-03-02 12:58:10 -0600

    Update canvas_get_course_roster.py
    - added a choice for enrollment status: active, invited, completed, deleted
    - slightly modified columnar formatting on final output

--- canvas/canvas_get_course_roster.py
+++ canvas/canvas_get_course_roster.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
-#
+
 import sys, os, requests, urllib, json, time, datetime
 from datetime import date
 from columnar import columnar
+from pprint import pprint
+sys.path.append("/var/lib/canvas-mgmt/bin")
 from canvasFunctions import *
 env = getEnv()
-logScriptStart()
+#logScriptStart()
 canvasApi = env["canvas.api-prod"]
 canvasToken = env["canvas.ro-api"]
 headers = {"Authorization": f"Bearer {canvasToken}"}
 params = {"per_page": 100}
+canvasAccountId = "1"
+workflowState = ''
+#params = {"per_page": 100, }
 canvasAccountId = "1"
 students = 0
 nonStudents = 0
@@ -18,14 +23,29 @@ today = str(date.today().strftime("%Y-%m-%d"))
 #
 print('')
 courseId = input("Enter the CourseID of the target course: ")
+print('')
+print("Enter enrollment state: (a)ctive,")
+print("                        (i)nvited,")
+print("                        (c)completed,")
+print("                        (d)eleted")
+while workflowState != 'a' and workflowState != 'i' and workflowState != 'c' and workflowState != 'd':
+    workflowState = input("                       > ").lower()[0]
+if workflowState == 'a': workflowState = 'active'
+elif workflowState == 'i': workflowState = 'invited'
+elif workflowState == 'c': workflowState = 'completed'
+else: workflowState = 'deleted'
+print('')
+#
+params = {"per_page": 100, "state": f"{workflowState}"}
+#
 canvasCourseId = findCanvasCourse(courseId)
 enrollUrl = f"{canvasApi}courses/{canvasCourseId}/enrollments"
-columnHeaders = ['sis_course_id', 'login_id', 'role']
+columnHeaders = ['course_id', 'section_id', 'net_id', 'role', 'current_grade']
 #
 response = requests.get(enrollUrl, headers=headers, params=params)
 #
 if len(response.json()) == 0:
-    print(f"Roster unavailable for:  {courseId}")
+    print(f"No {workflowState} enrollments found for:  {courseId}")
 else:
     tableTemp = []
     searchResults = response.json()
@@ -41,9 +61,10 @@ else:
             else:
                 nonStudents += 1
             #print(paginatedSearchResults[item]["sis_course_id"] + "|" + paginatedSearchResults[item]["user"]["login_id"] + "|" + paginatedSearchResults[item]["role"])
-            tableTemp.append([searchResults[item]['sis_course_id'], searchResults[item]['user']['login_id'], searchResults[item]['role'].replace('Enrollment','')])
+            tableTemp.append([searchResults[item]['sis_course_id'], searchResults[item]['sis_section_id'],searchResults[item]['user']['login_id'], searchResults[item]['role'].replace('Enrollment','')])
     table = columnar(tableTemp, columnHeaders, no_borders=True)
     print(table)
-    print("Students:     ", students)
-    print("Non-students: ", nonStudents)
+    print(f"Stats for {workflowState} enrollments: ")
+    print("  Students:     ", students)
+    print("  Non-students: ", nonStudents)
     print('')
