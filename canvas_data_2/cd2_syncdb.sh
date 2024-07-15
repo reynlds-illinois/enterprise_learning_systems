ae2739f0a9cd2b45af9c74ff69eab5099de0dfd7
Reynolds, Mark A <2440061+reynlds-illinois@users.noreply.github.com>
2024-07-15 08:40:10 -0500

    Update cd2_syncdb.sh
    Cleaned up and more concise

--- canvas_data_2/cd2_syncdb.sh
+++ canvas_data_2/cd2_syncdb.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
+# set LARGE temp location
+export TMPDIR='/var/lib/canvas-mgmt/tmp'
+# define targets
 today=$(date +%F)
+logLocation="/var/lib/canvas-mgmt/logs/"
 cd2LogLocation="/var/lib/canvas-mgmt/logs/cd2/"
-cd2LogFile="$cd2LogLocation$today.log"
-echo "LOG: $cd2LogFile"
+logFile=$logLocation$today".log"
+cd2LogFile=$cd2LogLocation$today"_syncdb.log"
+cd2ErrorFile=$cd2LogLocation$today"_syncdb_error.log"
+#echo "LOG: $cd2LogFile"
 now=$today" "$(date +%T)
 echo "$now | canvas-mgmt | cd2_dbsync.sh" >> $logFile
-# SYNC PRIMARY TABLES
-echo "======================== CD2 Canvas STARTING $now ========================" >> $cd2LogFile
-# python virtual env activation to enable the Instructure DAP client
+# activate python virtual env
 source /var/lib/canvas-mgmt/python39-venv/bin/activate
+# grab table listing
 TABLE_NAMES=$(dap list --namespace canvas)
+echo $TABLE_NAMES | tee -a $cd2LogFile
+sleep 2
+# loop through tables to sync them one at a time
 for TABLE in $TABLE_NAMES
 do
-    echo "Starting sync:  $TABLE"
-    dap syncdb --namespace canvas --table "$TABLE"
-    echo "Sync complete:  $TABLE"
+    echo "=== Processing table: $TABLE" | tee -a $cd2LogFile
+    now=$today" "$(date +%T)
+    echo "    = $now" | tee -a $cd2LogFile
+    echo "    = Starting syncdb:  $TABLE" | tee -a $cd2LogFile
+    echo "    = Command used: dap --loglevel debug --logfile $cd2LogFile syncdb --namespace canvas --table $TABLE" | tee -a $cd2LogFile
+    dap --loglevel debug --logfile "$cd2LogFile" syncdb --namespace canvas --table "$TABLE"
+    sleep 2
+    echo "==================================================" | tee -a $cd2LogFile
 done
-now=$today" "$(date +%T)
-echo "======================== CD2 Canvas COMPLETE $now ========================" >> $cd2LogFile
-# SYNC WEB LOG TABLE
-now=$today" "$(date +%T)
-echo "======================== CD2 Web Logs STARTING $now ========================" >> $cd2LogFile
-dap syncdb --namespace canvas_logs --table web_logs
-now=$today" "$(date +%T)
-echo "======================== CD2 Web Logs COMPLETE $now ========================" >> $cd2LogFile
-# deactivate python virtual env
+# disable python virtual env
 deactivate
+# look for any errors and notify
+grep 'Error\|ERROR' $cd2LogFile >> $cd2ErrorFile
+if [ -s "$cd2ErrorFile" ]; then
+    subjectLine=$today"_CD2_SYNCDB_ERRORS"
+    cat $cd2ErrorFile | mail -s $subjectLine MyUser@MyDomain.edu
+else
+    subjectLine=$today"_CD2_SYNCDB_PASS"
+    echo $subjectLine | mail -s $subjectLine MyUser@MyDomain.edu
+fi
