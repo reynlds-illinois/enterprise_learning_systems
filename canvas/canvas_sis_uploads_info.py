5097ec708ed9f9ae96e0fda1eeacacf7e2ecb7c3
Reynolds, Mark A. <2440061+reynlds-illinois@users.noreply.github.com>
2025-02-28 12:53:48 -0600

    Update canvas_sis_uploads_info.py
    Some small changes to add to productivity

--- canvas/canvas_sis_uploads_info.py
+++ canvas/canvas_sis_uploads_info.py
@@ -1,54 +1,61 @@
-###############################################################################
-# List most current 10 SIS uploads to Canvas
-###############################################################################
 #!/usr/bin/python
+#
 import sys, os, requests, urllib, json, time, datetime, tzlocal, pytz
 from columnar import columnar
 from datetime import date
 from pprint import pprint
 sys.path.append("/var/lib/canvas-mgmt/bin")
-from canvasFunctions import *
+from canvasFunctions import realm
 from columnar import columnar
-envDict = getEnv()
-env = 'x'
+from canvasFunctions import logScriptStart
+from canvasFunctions import canvasJsonDates
+#logScriptStart()
+realm = realm()
+print(f'Connected to {realm["envLabel"]} - {realm["canvasUrl"]}')
 #
-print('')
-while env != 'p' and env != 's':
-    env = input('Please enter the realm to use: (p)rod or (s)tage: ').lower().strip()
-    if env == 'p':
-        canvasApi = envDict["canvas.api-prod"]
-        canvasToken = envDict["canvas.write-token"]
-        envLabel = "PROD"
-    else:
-        canvasApi = envDict["canvas.api-beta"]
-        canvasToken = envDict["canvas.write-token"]
-        envLabel = "STAGE"
-print('')
-print(f"Connected to Canvas {envLabel}: {canvasApi}")
-print('')
-#
-canvasAccountId = "1"
+canvasApi = realm['canvasApi']
+canvasToken = realm['canvasToken']
+canvasEnvLabel = realm['envLabel']
 authHeaders = {"Authorization": f"Bearer {canvasToken}"}
 today = str(date.today().strftime("%Y-%m-%d"))
-params = {"per_page":10}
+canvasResults = 100.5
+importIDs = []
+urlDetail = '_.x'
+while True:
+    try:
+        print()
+        canvasResults = int(input('How many results to view: '))
+        if canvasResults > 0 and canvasResults < 51:
+            break
+        else:
+            print('> Too large. Please enter a whole number from 1 to 50.')
+    except ValueError:
+        print('> Not an integer. Please enter a whole number from 1 to 50.')
+print()
+params = {"per_page":canvasResults}
 imports = []
 timeFormat = "%Y-%m-%d  %H:%M:%S"
-importsHeader = ['ID', 'STATE', 'USER', 'FILE','CREATED', 'STARTED', 'ENDED', 'URL']
+importsHeader = ['ID', 'STATE', 'USER', 'FILE','CREATED', 'STARTED', 'ENDED']
+#importsHeader = 'ID      STATE      USER      FILE      CREATED      STARTED      ENDED'
 print('')
 #
 sisUrl = f"{canvasApi}accounts/1/sis_imports"
 r = requests.get(sisUrl, params=params, headers=authHeaders).json()
+r = canvasJsonDates(r)
 for item in r['sis_imports']:
     if item['started_at']:
-        importStartedAt = item['started_at'].replace("T"," ").replace("Z","")
+        #importStartedAt = item['started_at'].replace("T"," ").replace("Z","")
+        importStartedAt = item['started_at']
     else:
         importStartedAt = "None"
     if item['created_at']:
-        importCreatedAt = item['created_at'].replace("T"," ").replace("Z","")
+        importCreatedAt = item['created_at']
+        #importCreatedAt = item['created_at'].replace("T"," ").replace("Z","")
     else:
         importCreatedAt = "None"
     if item['ended_at']:
-        importEndedAt = item['ended_at'].replace("T"," ").replace("Z","")
+        importEndedAt = item['ended_at']
+        #importEndedAt = item['ended_at'].replace("T"," ").replace("Z","")
     else:
         importEndedAt = "None"
     if 'csv_attachments' in item.keys():
@@ -57,7 +64,25 @@ for item in r['sis_imports']:
     else:
         csvFilename = 'None'
         csvUrl = 'None'
-    imports.append([item['id'], item['workflow_state'], item['user']['login_id'], csvFilename, importCreatedAt, importStartedAt, importEndedAt, csvUrl])
-print(f'=== Most recent 10 SIS uploads for Canvas {envLabel}')
+    imports.append([item['id'], item['workflow_state'], item['user']['login_id'], csvFilename, importCreatedAt, importStartedAt, importEndedAt])
+    importIDs.append(str(item['id']))
+#
+#print(importIDs)
+print()
+print(f'> Most recent {canvasResults} SIS uploads for Canvas {canvasEnvLabel}')
 print(columnar(imports, importsHeader, no_borders=True))
-print('')
+print()
+#
+while urlDetail != 'q' and urlDetail not in importIDs:
+    urlDetail = input(str("Choose ID for additional detail or 'q' to quit: "))
+    print()
+if urlDetail == 'q':
+    print('Exiting without changes...')
+else:
+    importUrl = f"{sisUrl}/{urlDetail}"
+    r = requests.get(importUrl, headers=authHeaders).json()
+    r = canvasJsonDates(r)
+    print(f'=== DETAILS FOR IMPORT ID {urlDetail} ===')
+    print()
+    pprint(r)
+print()
