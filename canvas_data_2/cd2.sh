15b259ec472a382264854053f9c58e53b2d367a9
Reynolds, Mark A. <2440061+reynlds-illinois@users.noreply.github.com>
2025-04-11 11:18:55 -0500

    refactor for efficiency

--- canvas_data_2/cd2.sh
+++ canvas_data_2/cd2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -l
+
 # Set the TMPDIR location so you don't overwhelm the non-user space
 export TMPDIR='/var/lib/canvas-mgmt/tmp'
+
 # Check that arguments are provided and are correct...prompt and exit if not
 if [[ $# -ne 2 ]]; then
     echo '' >&2
@@ -26,30 +28,36 @@ case $2 in
                 echo '' >&2
                 exit 1
 esac
+
 # Set arguments to variables
 CD2_ACTION=$1
 CD2_LOG_LEVEL=$2
+
 # Configure other stuff (which may become arguments at some point)
 today=$(date +%F)
 now=$(date +%Y-%m-%d"_"%H:%M:%S)
+
 # These paths are specific to the host
 logLocation="/var/lib/canvas-mgmt/logs/"
 cd2LogLocation="/var/lib/canvas-mgmt/logs/cd2/"
 logFile=$logLocation$now".log"
 cd2LogFile=$cd2LogLocation$now"_"$CD2_ACTION".log"
-cd2ErrorFile=$cd2LogLocation$now"_"$CD2_ACTION"_error.log"
+
 echo "$now | canvas-mgmt | cd2_$CD2_ACTION.sh" >> $logFile
+
 # Enable the Python Virtual Environment
 source /var/lib/canvas-mgmt/python312-venv/bin/activate
+
 # Grab the current table names
-TABLE_NAMES=$(dap list --namespace canvas)
-echo $TABLE_NAMES | tee -a $cd2LogFile
+mapfile -t TABLE_NAMES < <(dap list --namespace canvas)
+echo "${TABLE_NAMES[@]}" | tee -a $cd2LogFile
 sleep 2
+
 # Now go do that voodoo that you do so well!!!
-for TABLE in $TABLE_NAMES
+for TABLE in "${TABLE_NAMES[@]}"
 do
     echo "=== Processing table: $TABLE" | tee -a $cd2LogFile
-    now=$today" "$(date +%T)
+    now=$(date +%Y-%m-%d" "%T)
     echo "    = $now" | tee -a $cd2LogFile
     echo "    = Starting $CD2_ACTION:  $TABLE" | tee -a $cd2LogFile
     echo "    = Command used: dap --loglevel $CD2_LOG_LEVEL --logfile $cd2LogFile $CD2_ACTION --namespace canvas --table $TABLE" | tee -a $cd2LogFile
@@ -57,14 +65,13 @@ do
     sleep 2
     echo "==================================================" | tee -a $cd2LogFile
 done
+
 # Disable the Python Virtual Environment
 deactivate
+
 # Send results
-grep 'Error\|ERROR' $cd2LogFile >> $cd2ErrorFile
-if [ -s "$cd2ErrorFile" ]; then
-    subjectLine=$today"_CD2_"$CD2_ACTION"_ERRORS"
-    cat $cd2ErrorFile | mail -s $subjectLine NOTIFYM3@illinois.edu
+if grep -q 'Error\|ERROR' "$cd2LogFile"; then
+    grep 'Error\|ERROR' "$cd2LogFile" | mail -s "${today}_CD2_${CD2_ACTION}_ERRORS" reynlds@illinois.edu
 else
-    subjectLine=$today"_CD2_"$CD2_ACTION"_PASS"
-    echo $subjectLine | mail -s $subjectLine NOTIFYM3@illinois.edu
+    echo "${today}_CD2_${CD2_ACTION}_PASS" | mail -s "${today}_CD2_${CD2_ACTION}_PASS" reynlds@illinois.edu
 fi
