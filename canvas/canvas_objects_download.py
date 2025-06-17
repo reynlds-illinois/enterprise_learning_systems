1ba1c079af8fca9423fbe8e353794a00bb46e67f
Reynolds, Mark A. <2440061+reynlds-illinois@users.noreply.github.com>
2025-06-17 12:36:03 -0500

    bringing it in sync with production

--- canvas/canvas_objects_download.py
+++ canvas/canvas_objects_download.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python
-#https://canvas.instructure.com/doc/api/account_reports.html#method.account_reports.available_reports
+
 import sys, os, requests, urllib, json, time, datetime, smtplib
 from datetime import date
 from email.mime.text import MIMEText
-from canvasFunctions import getEnv
-objectsList = ["users", "courses", "sections"]
-#canvasTerms = ["120228"]        # Optional...can set for active terms only
+sys.path.append("/var/lib/canvas-mgmt/bin")
+from canvasFunctions import *
+logScriptStart()
+objectsList = ["users", "courses", "sections", "accounts"]
 env = getEnv()
-canvasToken = env["canvas.ro-api"]
+canvasToken = env["canvas.ro-token"]
 canvasApi = env["canvas.api-prod"]
 canvasReportName = "provisioning_csv"
 canvasAccountId = "1"
 loopDelay = 20
 errors = 0
 today = str(date.today().strftime("%Y-%m-%d"))
-canvasObjectsPath = str("/my/canvas/config/")     # Target directory for downloaded files
+canvasObjectsPath = str("/var/lib/canvas-mgmt/config/")
 #
 for object in objectsList:
+    print(f"> Processing:  {object}")
+    print()
     try:
-        downloadFilePath = canvasObjectsPath + "/canvas_" + object + ".csv"
+        #emailResults = []
+        #emailResults.insert(f"=== {object} download starting")
+        ###print(f"=== {now()} {object} download starting")
+        #logFilePath = str(canvasObjectsPath + "/var/lib/canvas-mgmt/logs/" + object + "_" + today + ".log")
+        downloadFilePath = canvasObjectsPath + "canvas_" + object + ".csv"
         fileLink = ""
         reportProgress = 0
+        #
         # start and prep
         canvasEndpoint = f"accounts/{canvasAccountId}/reports/{canvasReportName}"
         reportUrl = urllib.parse.urljoin(canvasApi, canvasEndpoint)
         headers = {"Authorization": f"Bearer {canvasToken}"}
         params = {f"parameters[{object}]":"true"}
+        #
         # initiate report through api
         response = requests.post(reportUrl, headers=headers, params=params).json()
         reportId = response["id"]
@@ -36,25 +45,46 @@ for object in objectsList:
             response = requests.get(checkReportUrl, headers=headers)
             responseJson2 = json.loads(response.text)
             reportProgress = int(responseJson2['progress'])
+            ###print(f"checking {object} report progress. Currently: {reportProgress}")
             time.sleep(loopDelay)
+        #
         # extract file url and download file
         with open(downloadFilePath, "wb") as file:
             downloadFile = str(responseJson2["attachment"]["url"])
-            fileLink = requests.get(downloadFile, headers=headers)
+            fileLink = requests.get(downloadFile)
             file.write(fileLink.content)
+        #emailResults.insert(f"=== {object} download completed")
+        ###print(f"=== {now()} {object} download completed")
     except:
         errors += 1
+        #emailResults.append(f"!!! FAILED to download {object} file!!!")
+        ###print(f"!!! {now()} FAILED to download {object} file !!!")
 #
-emailFrom = "my_canvas@mydomain.com"
-emailRelay = "my_email_relay@mydomain.com"
+emailFrom = "canvas@illinois.edu"
+emailRelay = "express-smtp.cites.illinois.edu"
 if errors > 0:
     msg = MIMEText("Canvas Objects Download FAILED")
     msg['From'] = emailFrom
     msg["Subject"] = "Canvas Objects Download FAILED"
-    msg['To'] = 'canvas_mgr@mydomain.com'
-    emailTo = 'canvas_mgr@mydomain.com'
+    msg['To'] = 'reynlds@illinois.edu'
+    emailTo = 'reynlds@illinois.edu'
     S = smtplib.SMTP(emailRelay)
     S.sendmail(emailFrom, emailTo, msg.as_string())
-else:
-    msg = MIMEText("Canvas Objects Download COMPLETE")
-    msg["Subject"] = "Canvas Objects Download COMPLETE"
+    msg['To'] = 'ashbeal@illinois.edu'
+    emailTo = 'ashbeal@illinois.edu'
+    S = smtplib.SMTP(emailRelay)
+    S.sendmail(emailFrom, emailTo, msg.as_string())
+#else:
+    #msg = MIMEText("Canvas Objects Download COMPLETE")
+    #msg["Subject"] = "Canvas Objects Download COMPLETE"
+#for address in range(0, len(emailTo)):
+#msg['From'] = emailFrom
+#
+#msg['To'] = 'reynlds@illinois.edu'
+#emailTo = 'reynlds@illinois.edu'
+#S = smtplib.SMTP(emailRelay)
+#S.sendmail(emailFrom, emailTo, msg.as_string())
+#msg['To'] = 'ashbeal@illinois.edu'
+#emailTo = 'ashbeal@illinois.edu'
+#S = smtplib.SMTP(emailRelay)
+#S.sendmail(emailFrom, emailTo, msg.as_string())
