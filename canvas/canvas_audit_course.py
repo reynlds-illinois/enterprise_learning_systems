83dd6108d53313cd5f8c5e866f42ff081792d068
Mark Reynolds <reynlds@illinois.edu>
2025-08-14 09:40:40 -0500

    simplified the flow, added a column for source_course on course copies

--- canvas/canvas_audit_course.py
+++ canvas/canvas_audit_course.py
@@ -5,26 +5,28 @@ from pprint import pprint
 sys.path.append("/var/lib/canvas-mgmt/bin")
 from canvasFunctions import realm
 from canvasFunctions import findCanvasCourse
+from canvasFunctions import canvasCourseInfo
 from canvasFunctions import convertDate
+from canvasFunctions import canvasGetNetID
 from columnar import columnar
 
 # Initialize realm
 realm = realm()
 
 # Canvas API setup
-canvasApi = realm['canvasApi']
+canvasAPI = realm['canvasApi']
 canvasToken = realm['canvasToken']
 params = {"per_page": 100}
-headers = {"Authorization": f"Bearer {canvasToken}"}
+canvasAuth = {"Authorization": f"Bearer {canvasToken}"}
 
 # Get course ID from user
 uiucCourseID = input('Please enter the course ID:  ')
 print()
 canvasCourseID = findCanvasCourse(uiucCourseID)
-url = f"{canvasApi}audit/course/courses/{canvasCourseID}"
+url = f"{canvasAPI}audit/course/courses/{canvasCourseID}"
 
 # Fetch course audit data
-courseAudit = requests.get(url, headers=headers, params=params)
+courseAudit = requests.get(url, headers=canvasAuth, params=params)
 
 # Handle non-200 responses
 if courseAudit.status_code != 200:
@@ -47,47 +49,33 @@ if 'events' not in courseAudit or 'linked' not in courseAudit:
     sys.exit(1)
     print()
 
-courseEventsTable = []
-
 # Define event types and table headers
 eventTypes = ['concluded', 'created', 'copied_from', 'published', 'updated']
-courseEventsHeader = ['TIMESTAMP', 'COURSE_ID', 'EVENT_SRC', 'EVENT_TYPE', 'INITIATOR', 'DETAILS']
+courseEventsHeader = ['TIMESTAMP', 'COURSE_ID', 'EVENT_SRC', 'EVENT_TYPE', 'SOURCE_COURSE', 'INITIATOR', 'DETAILS']
+
+courseEventsTable = []
 
 # Process course events
 for item in courseAudit['events']:
-    if item['event_type'] in eventTypes:
-        userTemp = '-'
-        sourceCourseTemp = '-'
-        courseTemp = '-'
-
-        # Get user information
-        if item['links']['user']:
-            for row in courseAudit['linked']['users']:
-                if row['id'] == item['links']['user']:
-                    userTemp = row['sis_user_id']
-
-        # Get source course information
-        if item['event_type'] == 'copied_from':
-            for row in courseAudit['linked']['courses']:
-                if row['id'] == item['links']['copied_from']:
-                    sourceCourseTemp = row['sis_course_id']
-
-        # Get course information
-        if item['links']['course']:
-            for row in courseAudit['linked']['courses']:
-                if row['id'] == item['links']['course']:
-                    courseTemp = row['sis_course_id']
-
-        # Extract event details
-        createdAt = convertDate(item['created_at'])
-        eventSource = item['event_source']
-        eventType = item['event_type']
-        courseID = courseTemp
+    print('Parsing events...please wait...')
+    createdAt = convertDate(item['created_at'])
+    eventSource = item['event_source']
+    eventType = item['event_type']
+    if eventType == 'copied_from':
+        sourceCourseTemp = canvasCourseInfo(item['links']['copied_from'], canvasAPI, canvasAuth)['sis_course_id']
+    else: sourceCourseTemp = 'n/a'
+    if item['links']['course']:
+        courseTemp = canvasCourseInfo(item['links']['course'], canvasAPI, canvasAuth)['sis_course_id']
+    else: courseTemp = 'n/a'
+    if item['links']['user']:
+        userTemp = canvasGetNetID(item['links']['user'])[4]
+    else: userTemp = 'n/a'
+    if item['event_data']:
         eventData = item['event_data']
         eventData = '\n'.join([f'{k}: {v}' for k, v in eventData.items()])
-
-        # Append event to table
-        courseEventsTable.append([createdAt, courseID, eventSource, eventType, userTemp, eventData])
+    else: eventData = 'n/a'
+    # Append event to table
+    courseEventsTable.append([createdAt, courseTemp, eventSource, eventType, sourceCourseTemp, userTemp, eventData])
 
 # Print course events table
 if len(courseEventsTable) > 0:
