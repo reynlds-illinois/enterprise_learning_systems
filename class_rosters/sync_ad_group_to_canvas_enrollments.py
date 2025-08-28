4e76a9f4204ba3e763eae6e6a196a0577148d140
Mark Reynolds <reynlds@illinois.edu>
2025-08-28 09:48:39 -0500

    initial commit

--- class_rosters/sync_ad_group_to_canvas_enrollments.py
+++ class_rosters/sync_ad_group_to_canvas_enrollments.py
@@ -3,6 +3,7 @@
 import sys, requests
 from ldap3 import Server, Connection, ALL, NTLM, SUBTREE
 from columnar import columnar
+from pprint import pprint
 sys.path.append("/var/lib/canvas-mgmt/bin")
 from canvasFunctions import getEnv
 from canvasFunctions import realm
@@ -15,14 +16,11 @@ canvasAuth = {"Authorization": f"Bearer {canvasToken}"}
 ldapHost = env['UofI.ldap.ad_sys']
 ldapBindDn = env['UofI.ad_bind']
 ldapBindPw = env['UofI.ad_bindpwd']
-adGroupMembers = []
-canvasCourseEnrollees = []
+canvasCourseID = '41549'
+enrollmentHeader = ['NetID', 'Status', 'Enrollment_ID']
 canvasEnrollmentChanges = []
-canvasCourseID = '61284'
-enrollmentHeader = ['NetID', 'Status']
+userGroupDN = 'CN=canvas-mgr,OU=groups,OU=CANVAS,OU=CITES-Services,OU=CITES,OU=Urbana,DC=ad,DC=uillinois,DC=edu'
 proceed = ''
-#ldapRosterSearchBase = "OU=Class Rosters,OU=Register,OU=Urbana,DC=ad,DC=uillinois,DC=edu"
-#ldapUserSearchBase = "OU=Urbana,DC=ad,DC=uillinois,DC=edu"
 #
 def bind2Ldap(ldapHost, ldapBindDn, ldapBindPw):
     try:
@@ -31,14 +29,6 @@ def bind2Ldap(ldapHost, ldapBindDn, ldapBindPw):
         return ldapConn
     except:
         return False
-ldapConn = bind2Ldap(ldapHost, ldapBindDn, ldapBindPw)
-#
-#groupName = input('Enter the AD group name: ')
-#ldapUsersDir = 'OU=Users,DC=test,DC=local'
-#ldapUnitDir = 'OU=groups,OU=CANVAS,OU=CITES-Services,OU=CITES,OU=Urbana,DC=ad,DC=uillinois,DC=edu'
-userGroupDN = 'CN=canvas-mgr,OU=groups,OU=CANVAS,OU=CITES-Services,OU=CITES,OU=Urbana,DC=ad,DC=uillinois,DC=edu'
-#
-ldapConn.search(ldapUnitDir, '(cn={})'.format(userGroup))
 #
 def getADGroupDN(ldapConn, groupName):
     ldapConn.search(ldapUnitDir, '(cn={})'.format(groupName))
@@ -67,30 +57,47 @@ def canvasGetAllEnrollmentsInCourse(canvasAPI, canvasAuth, canvasCourseID):
         params = {}  # Only needed for first request
     return enrollments
 #
+ldapConn = bind2Ldap(ldapHost, ldapBindDn, ldapBindPw)
+#
 ADresponse = getADGroupMembers(ldapConn, userGroupDN)
+#print('=== ADresponse ===')
+#pprint(ADresponse)
+#print()
 CanvasResponse = canvasGetAllEnrollmentsInCourse(canvasAPI, canvasAuth, canvasCourseID)
-
+#print('=== CanvasResponse ===')
+#pprint(CanvasResponse)
+#print()
+#
 if len(ADresponse) > 0:
-    for user in response:
+    adGroupMembers = []
+    for user in ADresponse:
         adGroupMembers.append(user.split(',')[0].replace('CN=', ''))
         #print(user.split(',')[0].replace('CN=', ''))
-else: print("No users found in this group...")
+else: print("No users found in this AD group...")
 print()
 if len(CanvasResponse) > 0:
+    canvasCourseEnrollees = []
     for enrollment in CanvasResponse:
-        if enrollment['role'] == 'StudentEnrollment':
-            if enrollment['user']['sis_user_id']:
-                canvasCourseEnrollees.append([enrollment['user']['sis_user_id'], enrollment['id']])
-                #print(r['user']['sis_user_id'])
-
+        #if enrollment['role'] == 'StudentEnrollment':
+        if enrollment['user']['sis_user_id']:
+            canvasCourseEnrollees.append([enrollment['user']['sis_user_id'], enrollment['id']])
+            #print(r['user']['sis_user_id'])
+#
+addEnrollments = []
+dropEnrollments = []
+canvasEnrollmentChanges = []
 for netID in adGroupMembers:
-    if netID in canvasCourseEnrollees:
+    #print(netID)
+    if any(netID  == sublist[0] for sublist in canvasCourseEnrollees):
         canvasEnrollmentChanges.append([netID,'Enrolled - No change'])
-    if netID not in canvasCourseEnrollees:
-        canvasEnrollmentChanges.append([netID,'New Enrollment'])
+    if not any(netID  == sublist[0] for sublist in canvasCourseEnrollees):
+        canvasEnrollmentChanges.append([netID,'New'])
+        addEnrollments.append(netID)
 for userName in canvasCourseEnrollees:
-    if userName not in adGroupMembers:
-        canvasEnrollmentChanges.append([userName,'Drop Enrollment', userName[1]])
+    if userName[0] not in adGroupMembers:
+        canvasEnrollmentChanges.append([userName[0],'Drop Enrollment'])
+        dropEnrollments.append([userName[0], userName[1]])        
+canvasEnrollmentChanges.sort(key=lambda x: x[0])
 table = columnar(canvasEnrollmentChanges, enrollmentHeader, no_borders=True)
 print(table)
 print()
@@ -98,35 +105,34 @@ print()
 while proceed != 'y' and proceed != 'n':
     proceed = input('Proceed with these changes? (y/n): ').strip().lower()
 if proceed == 'y':
-    for change in canvasEnrollmentChanges:
-        if change[1] == 'New Enrollment':
-            print(f"Enrolling {change[0]}...")
-            # Course Enrollments
-            enrollURL = f"{canvasAPI}/courses/{canvasCourseID}/enrollments"
-            # Section Enrollments
-            #enrollURL = f"{canvasAPI}/sections/{sectionID}/enrollments"
-            enrollParams = {"enrollment[user_id]":change[0],
-                "enrollment[type]":"StudentEnrollment",
-                "enrollment[enrollment_state]":"active",
-                "enrollment[notify]":"false"}
-            r = requests.post(enrollURL, headers=canvasAuth, params=enrollParams)
-        elif change[1] == 'Drop Enrollment':
-            print(f"Dropping {change[0]}...")
-            # Course Enrollments
-            dropURL = f"{canvasAPI}/courses/{canvasCourseID}/enrollments/{change[2]}"
-            # Section Enrollments
-            #dropURL = f"{canvasAPI}/sections/{sectionID}/enrollments/{change[2]}"
-            dropParams = {"task":"conclude"}
-            r = requests.post(dropURL, headers=canvasAuth, params=dropParams)
+    # Add enrollments
+    for change in addEnrollments:
+        enrollURL = f"{canvasAPI}/courses/{canvasCourseID}/enrollments"
+        # Section Enrollments
+        #enrollURL = f"{canvasAPI}/sections/{sectionID}/enrollments"
+        enrollParams = {"enrollment[user_id]":f"sis_user_id:{change}",
+            "enrollment[type]":"StudentEnrollment",
+            "enrollment[enrollment_state]":"active",
+            "enrollment[notify]":"false"}
+        r = requests.post(enrollURL, headers=canvasAuth, params=enrollParams)
+        #pprint(r.text)
+        #input('enter to continue...')
+    for change in dropEnrollments:
+        dropURL = f"{canvasAPI}/courses/{canvasCourseID}/enrollments/{change[1]}"
+        # Section Enrollments
+        #dropURL = f"{canvasAPI}/sections/{sectionID}/enrollments/{change[2]}"
+        dropParams = {"task":"conclude"}
+        r = requests.delete(dropURL, headers=canvasAuth, params=dropParams)
+    print()
+    print(f'All changes are complete. Closing connection to {canvasAPI}')
+    print()
 elif proceed == 'n':
     print("No changes made.")
+    print()
 else:
     print("Invalid input. Please enter 'y' or 'n'.")
-
-if __name__ == '__main__':
-    groupDN = getADGroupDN(ldapConn, userGroup)
-    groupUSers = getADGroupMembers(ldapconn, groupDN)
-
-
-
-
+    print()
+#
+#if __name__ == '__main__':
+#    groupDN = getADGroupDN(ldapConn, userGroup)
+#    groupUSers = getADGroupMembers(ldapconn, groupDN)
