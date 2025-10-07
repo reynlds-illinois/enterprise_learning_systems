0eed9126207ae3260642c8846eb25e27df56877d
Mark Reynolds <reynlds@illinois.edu>
2025-10-07 10:14:37 -0500

    included the CD2 query and download to CSV

--- canvas/canvas_count_pdf_pages.py
+++ canvas/canvas_count_pdf_pages.py
@@ -1,20 +1,64 @@
 #!/usr/bin/python
 #
-import sys, requests, os, csv
+import sys, requests, os, csv,  datetime
 from pprint import pprint
 from PyPDF2 import PdfReader
 sys.path.append("/var/lib/canvas-mgmt/bin")
-from canvasFunctions import logScriptStart, realm
+from canvasFunctions import logScriptStart, realm, getEnv
 # Initialize environment
 #logScriptStart()
+envDict = getEnv()
 realm = realm()
 canvasToken = realm["canvasToken"]
 canvasApi = realm["canvasApi"]
-headers = {"Authorization": f"Bearer {canvasToken}"}
-params = {"per_page": 100}
 targetDir = '/var/lib/canvas-mgmt/tmp/pdf/'
 rCsvFile = '/var/lib/canvas-mgmt/tmp/canvas-pdf-pages.csv'
 sCsvFile = '/var/lib/canvas-mgmt/tmp/canvas-pdf-list.csv'
+now = datetime.datetime.now().timestamp()
+bannerTerms = envDict['banner.terms']
+print()
+while bannerTerm not in bannerTerms
+    bannerTerm = input('Enter the Banner term: ')
+    print()
+print()
+allPDFsFile = f'/var/lib/canvas-mgmt/tmp/pdf/all_pdfs_{bannerTerm}_{now}.csv'
+#
+def getAllPDFs(envDict, now, bannerTerm, allPDFsFile):
+    print()
+    print(f'  > querying CD2 for PDFs in Banner Term: {bannerTerm}')
+    print()
+    allPDFs = []
+    pgHost = envDict['cd2.pg.host']
+    pgPass = envDict['cd2.pg.pass']
+    pgUser = envDict['cd2.pg.user']
+    pgDb = envDict['cd2.pg.db']
+    pgPort = envDict['cd2.pg.port']
+    pgConn = connect2SQL(pgUser, pgPass, pgHost, pgPort, pgDb)
+    pgCursor = pgConn.cursor()
+    #print('> Connected to Canvas Data 2 Postgres.')
+    #headers = {"Authorization": f"Bearer {canvasToken}"}
+    #params = {"per_page": 100}
+    sqlQuery = f"""select c.sis_source_id, f.name, a.id, a.workflow_state, a.file_state, (a.size / 1024) as size_KB, a.display_name, 'https://canvas.illinois.edu/files/' || a.id || '/download?download_frd=1&verifier=' || a.uuid as DOWNLOAD_URL
+    from canvas.attachments a 
+    join canvas.courses c on c.id = a.context_id
+    join canvas.enrollment_terms et on et.id = c.enrollment_term_id
+    join canvas.folders f on f.id = a.folder_id  
+    where a.content_type = 'application/pdf'
+    and a.context_type = 'Course'
+    and c.sis_source_id is not null
+    and et.sis_source_id = '{bannerTerm}'
+    and a.file_state != 'deleted'
+    order by c.sis_source_id asc, a.display_name asc;"""
+    #
+    pgCursor.execute(pgQuery)
+    #columnHeaders = list([desc[0] for desc in pgCursor.description])
+    results = pgCursor.fetchall()
+    for row in results:
+        allPDFs.append([row])
+    with open(allPDFsFile, 'w') as exportFile:
+        write = csv.writer(exportFile, delimiter='|')
+        #write.writerow(columnHeaders)
+        write.writerows(results)
 #
 def downloadPDF(url, targetFilePath):
     response = requests.get(url, stream=True)
@@ -24,7 +68,7 @@ def downloadPDF(url, targetFilePath):
             if chunk:
                 f.write(chunk)
 #
-with open(rCsvFile, 'w', newline='') as resultsCsvFile:
+with open(allPDFsFile, 'w', newline='') as resultsCsvFile:
     fieldnames = ['numPages', 'totalSize', 'uiucCourseID', 'attachmentID', 'pdfName']
     writer = csv.DictWriter(resultsCsvFile, fieldnames=fieldnames)
     writer.writeheader()
