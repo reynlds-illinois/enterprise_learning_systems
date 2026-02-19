85ac961b2f4cdee253fb46953710d1b5fd44f2f5
Mark Reynolds <reynlds@illinois.edu>
2026-02-19 13:07:18 -0600

    more updates...

--- canvas/canvas_incident_mgmt.py
+++ canvas/canvas_incident_mgmt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-import sys, os, requests, json
+import sys, os, requests, json, pytz
 from datetime import datetime
 from pprint import pprint
 from columnar import columnar
@@ -21,8 +21,8 @@ query = {'api_version': 'V3-R1'}
 sr = []
 params = {}
 columnHeader = ['id', 'service', 'service_state', 'title', 'created', 'last_update', 'incident_status']
-incidentTypes = {'i': 'investigating', 'r': 'resolved', 'n': 'identified'}
-serviceStatusTypes = {'dp': 'degraded-performance', 'u': 'up', 'd': 'down'}#
+incidentTypes = {'i': 'investigating', 'r': 'resolved', 'n': 'identified', 'm': 'monitoring'}
+serviceStatusTypes = {'dp': 'degraded-performance', 'u': 'up', 'd': 'down'}
 #
 def getSingleIncident(baseURL, subDomain, apiKey, query, incidentID):
     requestHeaders = {'X-API-KEY': apiKey}
@@ -42,6 +42,8 @@ def getAllCanvasIncidents(baseURL, subDomain, apiKey, query):
             return json.loads(response.text)['data']
 #
 def createNewCanvasIncident(baseURL, subDomain, serviceID, query, requestHeaders, incidentTypes, serviceStatusTypes):
+    incidentTypeInput = ''
+    serviceStatusInput = ''
     url = f"{baseURL}/{subDomain}/incidents"
     incidentTitle = input('  = Please provide a short name for this Incident:  ')
     print()
@@ -55,27 +57,39 @@ def createNewCanvasIncident(baseURL, subDomain, serviceID, query, requestHeaders
             continue
     while True:
         try:
-            startTime = input('Enter a start time in 24-hour format (HH:MM): ')
+            startTime = input('Enter a start time in 24-hour format (HH:MM): ')   # fix this to take time in US Central
             datetime.strptime(startTime, '%H:%M').time()
             break
         except:
             print(startTime + ' is invalid. Please try again: '),
             continue
     while incidentTypeInput not in incidentTypes:
-        incidentTypeInput = input("  = Incident type: (i)nvestigating, (r)esolved, ide(n)tified ")
+        incidentTypeInput = input("  = Incident type: (i)nvestigating, (r)esolved, ide(n)tified, (m)onitoring > ")
         print()
     incidentType = str(incidentTypes.get(incidentTypeInput))
     while serviceStatusInput not in serviceStatusTypes:
-        serviceStatusInput = input("  = Current service status: (d)egraded performance, (u)p, (d)own ")
+        serviceStatusInput = input("  = Current service status: (dp)egraded performance, (u)p, (d)own ")
         print()
     serviceStatus = str(serviceStatusTypes.get(serviceStatusInput))
     payload = {"title": incidentTitle, 
+               "start_time": f"{startDate} {startTime}",
                "incident_type": incidentType,
                "service_statuses": [
-                   {"service_id": serviceID,
-                    "status_name": serviceStatus}]}
+                    {
+                        "service_id": serviceID,
+                        "status": serviceStatus
+                    }]
+                }
     response = requests.post(url, headers=requestHeaders, params=query, json=payload)
     if response.status_code == 200:
-        print('  = Incident created successfully!')
+        response = json.loads(response.text)
+        print('  = Incident created successfully:')
+        print()
+        print(f'  = ID:          {response['id']})
+        print(f'  = Title:       {response['title']})
+        print(f'  = Author:      {response['author']})
+        print(f'  = Start Time:  {response['start_time']})
+        print()
+        return response
     else:
         print('  = Something went wrong. Please try again.')
